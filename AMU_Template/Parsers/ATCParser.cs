de2231b0f92b7ee0c339ff7e16c04cbee2f2dffd
arno.muller <amuller@who.int>
2025-06-12 11:00:03 +0200

    Create ATC4 to ATC1 codes for Z99ZZ99 ATC5.

--- AMU_Template/Parsers/ATCParser.cs
+++ AMU_Template/Parsers/ATCParser.cs
@@ -32,9 +32,52 @@ namespace AMU_Template.Parsers
                 atc.ATCClass= StringParser.ParseAndTrimAndUpper(atcListRange.Cells[row, 4].Value);
                 atc.AMClass = StringParser.ParseAndTrimAndUpper(atcListRange.Cells[row, 5].Value);
                 atcList.Add(atc);
+                if (atc.Code == "Z99ZZ99")
+                {
+                    processZ99ATCs(atc, atcList);
+                }
             }
 
             return atcList;
+        }
+
+        private static void processZ99ATCs(ATC atc5, List<ATC> atcList)
+        {
+            // process ATC4
+            ATC atc4 = new ATC();
+            atc4.Code = atc5.Code.Substring(0, 5);
+            atc4.Name = atc5.Name;
+            atc4.Level = 4;
+            atc4.ATCClass = atc5.ATCClass;
+            atc4.AMClass = atc5.AMClass;
+            atcList.Add(atc4);
+
+            // process ATC3
+            ATC atc3 = new ATC();
+            atc3.Code = atc5.Code.Substring(0, 4);
+            atc3.Name = atc5.Name;
+            atc3.Level = 4;
+            atc3.ATCClass = atc5.ATCClass;
+            atc3.AMClass = atc5.AMClass;
+            atcList.Add(atc3);
+
+            // process ATC2
+            ATC atc2 = new ATC();
+            atc2.Code = atc5.Code.Substring(0, 3);
+            atc2.Name = atc5.Name;
+            atc2.Level = 4;
+            atc2.ATCClass = atc5.ATCClass;
+            atc2.AMClass = atc5.AMClass;
+            atcList.Add(atc2);
+
+            // process ATC2
+            ATC atc1 = new ATC();
+            atc1.Code = atc5.Code.Substring(0, 1);
+            atc1.Name = atc5.Name;
+            atc1.Level = 4;
+            atc1.ATCClass = atc5.ATCClass;
+            atc1.AMClass = atc5.AMClass;
+            atcList.Add(atc1);
         }
     }
 }
